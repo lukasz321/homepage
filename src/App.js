f02f59f72bc49b2f96cc587ede1651e3d4c1f110
Lukasz Zagaja <lukasz@density.io>
2023-10-08 15:11:41 -0400

    Added a link to YT channel, changed background gradient, cleanup.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { ReactComponent as DecisionsSvg } from "./svgs/decisions.svg";
 import { ReactComponent as WordleSvg } from "./svgs/wordle.svg";
 import { ReactComponent as DollarSvg } from "./svgs/dollar.svg";
 import { ReactComponent as TalkSvg } from "./svgs/talk.svg";
+import { ReactComponent as YoutubeSvg } from "./svgs/yt.svg";
 
 import "./App.css";
 
@@ -12,9 +13,9 @@ function App() {
         <a href="https://decide.lzagaja.com" target="_blank" rel="noreferrer">
           <div className="svg-container rotate-animation">
             <DecisionsSvg
-              fill="#ff2244"
-              stroke="#ff0000"
-              opacity="0.5"
+              fill="#ff44aa"
+              stroke="#ff44aa"
+              opacity="0.6"
               className="svg"
             />
           </div>
@@ -25,9 +26,6 @@ function App() {
             <TalkSvg opacity="0.8" className="svg" />
           </div>
         </a>
-
-        <div className="svg-container zoom-animation"></div>
-        <div className="svg-container zoom-animation"></div>
 
         <a href="https://wordle.lzagaja.com" target="_blank" rel="noreferrer">
           <div className="svg-container">
@@ -46,6 +44,14 @@ function App() {
               fill="#00ffaa"
               stroke="#00ffdd"
               opacity="0.64"
+              className="svg"
+            />
+          </div>
+        </a>
+        <a href="https://www.youtube.com/channel/UCsSeiZbxVfW8WlgDy7EKPfA" target="_blank" rel="noreferrer">
+          <div className="svg-container" style={{ width: "60%"}}>
+            <YoutubeSvg
+              opacity="0.70"
               className="svg"
             />
           </div>
