d874ea7ec90e4743e59e7d827bc10f42d5c100b2
Lukasz Zagaja <lukasz@density.io>
2023-12-03 23:52:23 -0500

    New icons, new layout

--- src/App.js
+++ src/App.js
@@ -1,59 +1,42 @@
-import { ReactComponent as DecisionsSvg } from "./svgs/decisions.svg";
-import { ReactComponent as WordleSvg } from "./svgs/wordle.svg";
-import { ReactComponent as DollarSvg } from "./svgs/dollar.svg";
-import { ReactComponent as TalkSvg } from "./svgs/talk.svg";
-import { ReactComponent as YoutubeSvg } from "./svgs/yt.svg";
+import { ReactComponent as TreasurySvg } from "./svg/treasury.svg";
+import { ReactComponent as PowerplotSvg } from "./svg/powerplot.svg";
+import { ReactComponent as YoutubeSvg } from "./svg/yt.svg";
+import { ReactComponent as GithubSvg } from "./svg/github.svg";
+import { ReactComponent as EmailSvg } from "./svg/email.svg";
+import { ReactComponent as InfoSvg } from "./svg/about.svg";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="container">
-      <div className="header">Header items go here. Github, contact, etc.</div>
+      <div className="header">
+        <a href="" target="_blank" rel="noreferrer">
+          <InfoSvg />
+        </a>
+        <a href="https://github.com/lukasz321" target="_blank" rel="noreferrer">
+          <GithubSvg />
+        </a>
+        <a href="mailto:hello@lzagaja.com">
+          <EmailSvg />
+        </a>
+      </div>
+
       <div className="grid-container">
         <a href="https://treasuryields.com" target="_blank" rel="noreferrer">
-            <DollarSvg
-              fill="#00ffaa"
-              opacity="0.84"
-            />
+          <TreasurySvg />
         </a>
-        <a href="https://www.youtube.com/channel/UCsSeiZbxVfW8WlgDy7EKPfA" target="_blank" rel="noreferrer">
-            <YoutubeSvg
-              opacity="0.70"
-              className="svg"
-            />
+        <a href="https://power.lzagaja.com" target="_blank" rel="noreferrer">
+          <PowerplotSvg />
         </a>
-      {/*
-      <div className="container">
-        <a href="https://decide.lzagaja.com" target="_blank" rel="noreferrer">
-          <div className="svg-container rotate-animation">
-            <DecisionsSvg
-              fill="#ff44aa"
-              stroke="#ff44aa"
-              opacity="0.6"
-              className="svg"
-            />
-          </div>
+        <a
+          href="https://www.youtube.com/channel/UCsSeiZbxVfW8WlgDy7EKPfA"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <YoutubeSvg />
         </a>
-
-        <a href="https://text.lzagaja.com" target="_blank" rel="noreferrer">
-          <div className="svg-container">
-            <TalkSvg opacity="0.8" className="svg" />
-          </div>
-        </a>
-
-        <a href="https://wordle.lzagaja.com" target="_blank" rel="noreferrer">
-          <div className="svg-container">
-            <WordleSvg
-              className="svg"
-              fill="#ffcc44"
-              stroke="#ffcc44"
-              opacity="0.7"
-            />
-          </div>
-        </a>
-        */}
-    </div>
+      </div>
     </div>
   );
 }
